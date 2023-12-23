3ed11f88de6c942b0e180e89a1f02fb38447ecac
MI Sajid <misajidsa@gmail.com>
2023-12-23 18:03:15 +0600

    build : project created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className='p-5'>
+      <strong className='text-2xl block'>Technologies</strong><br />
+      <ul className='list-disc pl-5'>
+        <li>Chakra-UI</li>
+        <li>Tailwind CSS</li>
+        <li>Vite-React</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
