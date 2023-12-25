b1fcce6c8ea73bd079437bf7d6f3c0448afb4892
MI Sajid <misajidsa@gmail.com>
2023-12-26 00:05:36 +0600

    made navbar and chat

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -1,7 +1,51 @@
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { MdSearch } from 'react-icons/md';
+import { FaRegBell } from 'react-icons/fa';
+
 const Chat = () => {
     return (
-        <div>
-            <div>Chat</div>
+        <div className="grow flex overflow-hidden">
+            <div className="w-[330px] shadow-[0px_0px_3px_#00000040] p-3">
+                <InputGroup>
+                    <InputLeftElement paddingLeft={3}>
+                        <MdSearch color="#808080" size="25px" />
+                    </InputLeftElement>
+                    <Input color="#707070" paddingBottom="2px" placeholder="Search..." borderRadius="100" />
+                </InputGroup>
+                <div className="flex items-center justify-between py-3 px-1">
+                    <h1 className="text-xl font-medium">Messages</h1>
+                    <FaRegBell size="20px" className="rotate-45" />
+                </div>
+                <div className='flex flex-col gap-3'>
+                    <div className="accent-bg shadow-[0px_0px_2px_#00000060] h-16 rounded-lg p-2 flex gap-2">
+                        <div className="overflow-hidden aspect-square h-full rounded-full">
+                            <img className="block w-full h-full object-cover" src="https://img.freepik.com/premium-photo/handsome-young-businessman-shirt-eyeglasses_85574-6228.jpg" />
+                        </div>
+                        <div className='flex flex-col justify-center'>
+                            <h1 className="font-medium text-gray-800">Darshan Zalavadiya</h1>
+                            <span className='text-sm block'>How are you?</span>
+                        </div>
+                    </div>
+                    <div className="accent-bg shadow-[0px_0px_2px_#00000060] h-16 rounded-lg p-2 flex gap-2">
+                        <div className="overflow-hidden aspect-square h-full rounded-full">
+                            <img className="block w-full h-full object-cover" src="https://img.freepik.com/premium-photo/handsome-young-businessman-shirt-eyeglasses_85574-6228.jpg" />
+                        </div>
+                        <div className='flex flex-col justify-center'>
+                            <h1 className="font-medium text-gray-800">Darshan Zalavadiya</h1>
+                            <span className='text-sm block'>How are you?</span>
+                        </div>
+                    </div>
+                    <div className="accent-bg shadow-[0px_0px_2px_#00000060] h-16 rounded-lg p-2 flex gap-2">
+                        <div className="overflow-hidden aspect-square h-full rounded-full">
+                            <img className="block w-full h-full object-cover" src="https://img.freepik.com/premium-photo/handsome-young-businessman-shirt-eyeglasses_85574-6228.jpg" />
+                        </div>
+                        <div className='flex flex-col justify-center'>
+                            <h1 className="font-medium text-gray-800">Darshan Zalavadiya</h1>
+                            <span className='text-sm block'>How are you?</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className="h-screen w-14 p-2 shadow-[0px_1px_4px_#00000070] flex flex-col items-center justify-between">
+        <div className="accent-bg h-screen w-14 p-2 shadow-[0px_1px_4px_#00000070] flex flex-col items-center justify-between">
             <img src="logo.jpg" alt="SkillSwap" />
             <div className="flex flex-col gap-4 items-center">
                 <NavLink to="/" className={({ isActive }) => (isActive ? 'link-active' : '')}>
