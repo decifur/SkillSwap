2fce2eeeba8232f5f33f20fad9c3fbe2a0c4dddd
MI Sajid <misajidsa@gmail.com>
2023-12-26 07:06:20 +0600

    added two new reducers

--- src/slice/userSlice.js
+++ src/slice/userSlice.js
@@ -1,54 +1,59 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  user: {
-    info: {
-      firstName: "Jessica",
-      lastName: "Sympson",
-      username: "jessica420",
-      designation: "software Engineer",
-      country: "India",
-      imageLink:
-        "https://img.freepik.com/premium-photo/girl-red-sweater-with-red-top_81048-3567.jpg?size=626&ext=jpg",
-      theme: "light",
+    user: {
+        info: {
+            firstName: "Jessica",
+            lastName: "Sympson",
+            username: "jessica420",
+            designation: "software Engineer",
+            country: "India",
+            imageLink: "https://img.freepik.com/premium-photo/girl-red-sweater-with-red-top_81048-3567.jpg?size=626&ext=jpg",
+            theme: "light",
+        },
+        skills: [
+            {
+                name: "React",
+                level: "beginner",
+                rating: 2,
+            },
+            {
+                name: "Angular",
+                level: "intermediate",
+                rating: 4,
+            },
+        ],
     },
-    skills: [
-      {
-        name: "React",
-        level: "beginner",
-        rating: 2,
-      },
-      {
-        name: "Angular",
-        level: "intermediate",
-        rating: 4,
-      },
-    ],
-  },
 };
 
 export const UserSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    toggleTheme: (state) => {
-      if (state.user.info.theme === "light") {
-        state.user.info.theme = "dark";
-      } else {
-        state.user.info.theme = "light";
-      }
-      localStorage.setItem("applied-theme",state.user.info.theme);
+    name: "user",
+    initialState,
+    reducers: {
+        toggleTheme: (state) => {
+            state.user.info.theme = state.user.info.theme === "light" ? "dark" : "light";
+            localStorage.setItem("applied-theme", state.user.info.theme);
+        },
+        setTheme: (state, action) => {
+            if (action.payload) {
+                state.user.info.theme = action.payload;
+            } else {
+                localStorage.setItem("applied-theme", "light");
+            }
+        },
+        updateUserInfo: (state, action) => {
+            state.user.info = { ...state.user.info, ...action.payload };
+        },
+        updateUserSkills: (state, action) => {
+            let matchFound = false;
+            state.user.skills.forEach((e, i) => {
+                e.name == action.payload.name && ((state.user.skills[i] = { ...state.user.skills[i], ...action.payload }), (matchFound = true));
+            });
+            !matchFound && state.user.skills.push(action.payload);
+        },
     },
-    setTheme:(state,action)=>{
-        if(action.payload){
-            state.user.info.theme = action.payload;
-        }else{
-            localStorage.setItem("applied-theme",'light')
-        }
-    }
-  },
 });
 
-export const { toggleTheme , setTheme } = UserSlice.actions;
+export const { toggleTheme, setTheme, updateUserSkills, updateUserInfo } = UserSlice.actions;
 
 export default UserSlice.reducer;
