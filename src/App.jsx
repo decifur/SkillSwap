1135f3ffb134389953e4444e6f1078ce5334c57d
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2023-12-27 18:48:37 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Matchmaking from './Pages/Matchmaking';
 import Profile from './Pages/Profile';
 import Settings from './Pages/Settings';
+import Login from './Pages/login';
 
 function Layout() {
     return (
@@ -18,6 +19,7 @@ function Layout() {
 const App = () => {
     return (
         <Routes>
+            <Route path="/login" element={<Login />} />
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
                 <Route path="/matchmaking" element={<Matchmaking />} />
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -2,7 +2,7 @@ import { MdEdit } from "react-icons/md";
 const Profile = () => {
   return (
     <>
-     <div className="w-full h-full bg-gray-100 p-7 ">
+     <div className="w-full h-full box p-7 ">
         <div className="flex text-center"> 
           <img src="/public/logo.jpg" width={50} alt="" />
           <h1 className="text-3xl font-extrabold py-0 text-center px-4 ">your profile</h1>
@@ -35,20 +35,20 @@ const Profile = () => {
             <samp ><MdEdit/></samp>
           </div>
           <div className="w-8/12 flex flex-wrap gap-3">
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 1</button>
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 2</button>
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 3</button>
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 4</button>
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 5</button>
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 6</button>
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 7</button> 
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 8</button>
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 9</button>
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 10</button>
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 11</button>
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 12</button>
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 13</button>
-              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 14</button>
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 1</button>
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 2</button>
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 3</button>
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 4</button>
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 5</button>
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 6</button>
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 7</button> 
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 8</button>
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 9</button>
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 10</button>
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 11</button>
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 12</button>
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 13</button>
+              <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-blue-500 border-r-blue-400 rounded-3xl border-2"> SKILLS 14</button>
          
           </div>
         </div>
