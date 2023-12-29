65604f097dd48d019117ef52d7dcbd280531bb1b
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-12-29 21:40:46 +0530

    updated logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div className="accent-bg h-screen w-14 p-2 shadow-[0px_1px_4px_#00000070] flex flex-col items-center justify-between">
-            <img src="logo.jpg" alt="SkillSwap" />
+            <img src="logo.png" alt="SkillSwap" />
             <div className="flex flex-col gap-4 items-center">
                 <NavLink to="/" className={({ isActive }) => (isActive ? 'active-link' : '')}>
                     <GoHome className="m-[6px]" size="25px" />
